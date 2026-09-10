ac2e3ee2ea06aabde6bd939c32f71db2e2c7f6d6
KaLLeN07823 <2037287931@qq.com>
2026-09-10 10:27:28 +0800

    新增第一周Python练习

---
+++ week01/day01.py
@@ -0,0 +1,51 @@
+# # print('hello, world')
+# # print('The quick brown fox','jumps over','the lazy dog')
+# # print(300)
+# # print(100+200)
+# # print('100+200 =',100+200)
+# #
+# # print('1024*768=',1024*768)
+# # a = 100
+# # if a >= 0:
+# #     print(a)
+# # else:
+# #     print(-a)
+# #
+# #
+# # print('I\'m ok.')
+# # print('I\'m learning\nPython.')
+# # print('\\\n\\')
+# # print('\\\t\\')
+# # print(r'\\\t\\')
+# # print('''line1
+# # line2
+# # line3''')
+# print(r'''hello,\n
+# world''')
+# # age = int(input('请输入年龄:'))
+# # if age >= 18:
+# #     print('adult')
+# # else:
+# #     print('teenager')
+# # a = 123
+# # print(a)
+# # a = 'ABC'
+# # print(a)
+# #
+# #
+# # a = 'ABC'
+# # b = a
+# # a = 'XYZ'
+# # print(b)
+# # print(a)
+# n = 123
+# print(n)
+# f = 456.789
+# print(f)
+# s1 = 'Hello, world'
+# s2 = 'Hello, \'Adam\''
+# s3 = r'Hello, "Bart"'
+# s4 = (r'''Hello,
+# Bob!''')
+# print(s1, s2, s3, s4, sep='\n')
+print('包含中文的str')
