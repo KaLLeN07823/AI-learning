d00db11e8498378008f84029b71ba2d07aab1e83
KaLLeN07823 <2037287931@qq.com>
2026-09-10 14:02:07 +0800

    新增第一周Python练习

--- week01/day02.py
+++ week01/day02.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+print('%2d-%02d' % (3,1))
+print('%.2f' % 3.1415926)
+# s1 = 72
+# s2 = 85
+# r = (s2-s1)/s1
+# print(f'{r:.1f%}')
+w1 = 60.0
+w2 = 63.6
+r = (w2-w1)/w1
+# print('%.1f%%' % (r * 100))
+print(f'{r:.1%}')
