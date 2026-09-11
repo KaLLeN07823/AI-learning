0c7ff48170dc5d29c8183cdf81608e675d0e3220
KaLLeN07823 <2037287931@qq.com>
2026-09-11 12:22:33 +0800

    新增第一周Python练习

--- week01/practice.py
+++ week01/practice.py
@@ -1,1 +1,30 @@
-print("我第一次用 PyCharm 上传代码 人才")
+# name = input ("请输入姓名:")
+# school = input("请输入学校:")
+# specialty = input("请输入专业:")
+# print(f"大家好,我叫{name},来自{school},专业是{specialty}.")
+# number1 = int (input("请输入第一个数字:"))
+# number2 = int (input("请输入第二个数字:"))
+# total = number1 + number2
+# product = number1 * number2
+# print("它们的和是:",number1 + number2)
+# print("它们的乘积是:",number1 * number2)
+# print(f"它们的和是:{total}")
+# print(f"它们的乘积是:{product}")
+# length = float(input("请输入长方形的长:"))
+# wide = float(input("请输入长方形的宽:"))
+# area = length * wide
+# perimeter = (length + wide) * 2
+# print(f"长方形的面积是:{area}")
+# print(f"长方形的周长是:{perimeter}")
+# name = input("请输入姓名:")
+# before_age = int (input("请输入年龄:"))
+# future_age = before_age + 5
+# print(f"{name},你5年后是{future_age}岁")
+# commodity = input("请输入商品:")
+# unit_price = float(input("请输入单价:"))
+# amount = int(input("请输入数量:"))
+# total_prices = amount * unit_price
+# print(f"你购买了{amount}瓶{commodity},总价是{total_prices}元.")
+# age = int(input("请输入年龄:")) #原代码报错的原因是input仅仅读取字符串 本质字符串＋一个整数是错误的 所以要用int转换
+# future_age = age + 5
+# print(f"你5年后是{future_age}岁")
