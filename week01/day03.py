c375f914f1adcd22be3fc1763720209e27d8fab8
KaLLeN07823 <2037287931@qq.com>
2026-09-12 15:42:58 +0800

    新增第一周Python练习

---
+++ week01/day03.py
@@ -0,0 +1,8 @@
+# classmates = ['Michael', 'Bob', 'Tracy', 'Eleven', 'Mike']
+# print('\n'.join(classmates)
+# list = [['Apple', 'Google', 'Microsoft'],
+#         ['Java', 'Python', 'Ruby', 'PHP'],
+#         ['Adam', 'Bart', 'Bob']]
+# print(list[0][0])
+# print(f"{list[1][1]}")
+# print(f"{list[2][2]}")
