c593d6545ab12b98ac24e85e33714f8f7a4b0751
KaLLeN07823 <2037287931@qq.com>
2026-09-20 15:48:25 +0800

    新增第一周Python练习

---
+++ week01/day06.py
@@ -0,0 +1,11 @@
+# names = ['Michael', 'Bob', 'Tracy']
+# scores = [95, 75, 85]
+# t1 = (1,2,3)
+# s = {t1}
+# print(s)
+# t1 = (1,2,3)
+# d = {t1:"hello"}
+# print(d)
+# t2 =(1,[2,3])
+# s = {t2}
+# print(s)
